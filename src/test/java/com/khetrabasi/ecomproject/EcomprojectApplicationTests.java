774ea5839c0d424e96c6b0a6defff08cf1e2e77e
KhetrabasiReddy <reddykhetrabasi@gmail.com>
2025-02-11 15:35:34 +0530

    first commit

---
+++ src/test/java/com/khetrabasi/ecomproject/EcomprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khetrabasi.ecomproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
